2dd929d870d7e17d537d694a90ec2a850fe0f385
issamehassane <issame.hassane@hotmail.com>
2025-07-23 10:19:51 +0200

    Push main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,28 @@
 
 @main
 def main(): Unit = {
-  println("hello world")
-}
+  var user : user = null
+  var continue = true
+
+  while(continue){
+    if(user != null) {
+      println("inscription")
+      println("connexion")
+    }
+    else
+    {
+      val menu = List("Utilisateur", "Reservation", "Messagerie")
+
+      println("Menu principal :")
+      for ((section, index) <- menu.zipWithIndex) {
+        println(s"${index + 1} : $section")
+      }
+      
+      switch case
+
+      }
+  }
+
+
+  }
+
