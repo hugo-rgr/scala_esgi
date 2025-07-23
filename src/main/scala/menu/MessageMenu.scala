3c30efcb8bef945fbcb611048ed164741a9386d1
thewhiskas27 <daromir63@gmail.com>
2025-07-23 14:57:21 +0200

    MessageMenu

--- src/main/scala/menu/MessageMenu.scala
+++ src/main/scala/menu/MessageMenu.scala
@@ -7,68 +7,26 @@ object MessageMenu
   import models.User
   import scala.io.StdIn
 
-  def display(): Unit = {
-    var user: User = null
-    var continue = true
+  def afficherMenu(user: User): Unit = {
+    var continuer = true
 
-    val statement = DBConnection.connection
+    while (continuer) {
+      println("\n| Mes trajets")
+      println("1. Nouveau trajet (conducteur)")
+      println("2. Trajets à venir")
+      println("3. Trajets passés")
+      println("4. Retour au menu principal")
 
-    while (continue) {
-      if (user == null) {
-        println("|Menu d'authentification")
-        println("1: Envoyer un message")
-        println("2: Messages reçus")
-        println("3: Retour au menu")
+      println("\nSelectionnez une action")
+      val choix = StdIn.readInt()
 
-        println("Selectionnez une action")
-        val choix = StdIn.readInt()
-
-        choix match {
-          case 1 =>
-            user = dao.userInscription()
-            if (user == null)
-              println("/!\\ Erreur lors de l'inscription")
-            else
-              println("///Inscription reussie")
-          case 2 =>
-            user = dao.userConnexion()
-            if (user == null)
-              println("/!\\ Nom d'utilisateur ou mots de passe incorect ")
-            else
-              println("///Connexion reussie")
-          case 3 =>
-            println("A bientôt")
-            continue = false
-
-          case _ =>
-            println("Commande invalide !")
-        }
-      }
-      else {
-        val menu = List("Rechercher un trajet", "Mes trajets", "Mes messages", "Mon compte", "Quitter")
-
-        println("Menu principal :")
-        for ((section, index) <- menu.zipWithIndex) {
-          println(s"${index + 1} : $section")
-        }
-
-        println("Selectionnez une action")
-        val choix = StdIn.readInt()
-
-        choix match {
-          case 1 =>
-            println("Utilisateur")
-
-          case 2 =>
-            println("Reservation")
-
-          case 3 =>
-            println("Messagerie")
-        }
+      choix match {
+        case 1 => ecrireMessage(user)
+        case 2 => afficherMessages(user)
+        case 3 => chercherMessage(user)
+        case 4 => continuer = false
+        case _ => println("Commande invalide !")
       }
     }
-
-
   }
-
 }
