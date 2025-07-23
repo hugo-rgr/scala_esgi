5c91adf2edc7bc3476be598f9a972adf11e225f0
issamehassane <issame.hassane@hotmail.com>
2025-07-23 10:35:17 +0200

    Push structure principal

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,28 +1,50 @@
+import scala.io.StdIn
 
 @main
 def main(): Unit = {
-  var user : user = null
+  var user: user = null
   var continue = true
 
-  while(continue){
-    if(user != null) {
-      println("inscription")
-      println("connexion")
+  while (continue) {
+    if (user != null) {
+      println("Inscription")
+      println("Connexion")
+
+      println("Selectionnez une action")
+      val choix = StdIn.readInt()
+
+      choix match {
+        case 1 =>
+          println("Inscription")
+
+        case 2 =>
+          println("Connexion")
+      }
     }
-    else
-    {
+    else {
       val menu = List("Utilisateur", "Reservation", "Messagerie")
 
       println("Menu principal :")
       for ((section, index) <- menu.zipWithIndex) {
         println(s"${index + 1} : $section")
       }
-      
-      switch case
 
+      println("Selectionnez une action")
+      val choix = StdIn.readInt()
+
+      choix match {
+        case 1 =>
+          println("Utilisateur")
+
+        case 2 =>
+          println("Reservation")
+
+        case 3 =>
+          println("Messagerie")
       }
+    }
   }
 
 
-  }
+}
 
---
+++ src/main/scala/dao/Default.scala
@@ -0,0 +1,3 @@
+package dao
+
+object Default
