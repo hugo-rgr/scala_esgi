acff32025d36174cd298643fe8b7d19b44be9d92
thewhiskas27 <daromir63@gmail.com>
2025-07-23 15:33:53 +0200

    Continuation du msgmenu

--- src/main/scala/dao/UserDAO.scala
+++ src/main/scala/dao/UserDAO.scala
@@ -3,78 +3,82 @@ import Utils.DBConnection
 import models.User
 import scala.io.StdIn
 
-def userConnexion(): User = {
-  println("|Connexion")
+object UserDAO {
 
-  println("Identifiant :")
-  val identifiant = StdIn.readLine()
-  println("Mot de passe:")
-  val password = StdIn.readLine()
+  def userConnexion(): User = {
+    println("|Connexion")
 
-  val requete = "SELECT * FROM USER WHERE user_name = ?"
+    println("Identifiant :")
+    val identifiant = StdIn.readLine()
+    println("Mot de passe:")
+    val password = StdIn.readLine()
 
-  val statement = DBConnection.connection.prepareStatement(requete)
-  statement.setString(1, identifiant)
+    val requete = "SELECT * FROM USER WHERE user_name = ?"
 
-  val result = statement.executeQuery()
+    val statement = DBConnection.connection.prepareStatement(requete)
+    statement.setString(1, identifiant)
 
-  if (result.next()) {
-    if (result.getString("user_hash_pwd") == password) {
-      val user = User(
-        userId = result.getInt("user_id"),
-        nom = result.getString("user_name"),
-        vehicule = result.getString("user_vehicule"),
-        note = result.getInt("user_note"),
-        nombreNote = result.getInt("user_nb_notes")
-      )
-      user
+    val result = statement.executeQuery()
+
+    if (result.next()) {
+      if (result.getString("user_hash_pwd") == password) {
+        val user = User(
+          userId = result.getInt("user_id"),
+          nom = result.getString("user_name"),
+          vehicule = result.getString("user_vehicule"),
+          note = result.getInt("user_note"),
+          nombreNote = result.getInt("user_nb_notes")
+        )
+        user
+      } else {
+        null
+      }
     } else {
       null
     }
-  } else {
-    null
-  }
-}
-
-def userInscription(): User = {
-  println("|Inscription")
-  var identifiant: String = null
-  var password: String = null
-  var vehicule: String = null
-
-  println("Identifiant :")
-  identifiant = StdIn.readLine()
-
-  var passwordOk = false
-  while(!passwordOk) {
-    println("Mot de passe:")
-    password = StdIn.readLine()
-    println("Taper de nouveau le mots de passe")
-    if (password == StdIn.readLine())
-      passwordOk = true
   }
 
-  println("Vehicule")
-  vehicule = StdIn.readLine()
+  def userInscription(): User = {
+    println("|Inscription")
+    var identifiant: String = null
+    var password: String = null
+    var vehicule: String = null
 
-  val requeteInsert = "INSERT INTO BLABLACAR.`User`" +
-    "(user_name, user_hash_pwd, user_vehicule, user_note, user_nb_notes)" +
-    "VALUES(?, ?, ?, 0, 0);"
+    println("Choisissez votre identifiant :")
+    identifiant = StdIn.readLine()
 
-  val statementInsert = DBConnection.connection.prepareStatement(requeteInsert)
-  statementInsert.setString(1,identifiant)
-  statementInsert.setString(2,password)
-  statementInsert.setString(3,vehicule)
-  statementInsert.executeUpdate()
+    var passwordOk = false
+    while (!passwordOk) {
+      println("Choisissez votre mot de passe:")
+      password = StdIn.readLine()
+      println("Confirmez votre mot de passe")
+      if (password == StdIn.readLine())
+        passwordOk = true
+      else
+        println("/!\\Les mots de passe ne correspondent pas")
+    }
 
-  val requeteSelect = "SELECT * FROM USER WHERE user_name = ?"
+    println("Vehicule")
+    vehicule = StdIn.readLine()
 
-  val statementSelect = DBConnection.connection.prepareStatement(requeteSelect)
-  statementSelect.setString(1, identifiant)
+    val requeteInsert = "INSERT INTO BLABLACAR.`User`" +
+      "(user_name, user_hash_pwd, user_vehicule, user_note, user_nb_notes)" +
+      "VALUES(?, ?, ?, 0, 0);"
 
-  val result = statementSelect.executeQuery()
+    val statementInsert = DBConnection.connection.prepareStatement(requeteInsert)
+    statementInsert.setString(1, identifiant)
+    statementInsert.setString(2, password)
+    statementInsert.setString(3, vehicule)
+    statementInsert.executeUpdate()
 
-  if (result.next()) {
+    val requeteSelect = "SELECT * FROM USER WHERE user_name = ?"
+
+    val statementSelect = DBConnection.connection.prepareStatement(requeteSelect)
+    statementSelect.setString(1, identifiant)
+
+    val result = statementSelect.executeQuery()
+
+    if (result.next()) {
       val user = User(
         userId = result.getInt("user_id"),
         nom = result.getString("user_name"),
@@ -83,11 +87,148 @@ def userInscription(): User = {
         nombreNote = result.getInt("user_nb_notes")
       )
       user
+    }
+    else {
+      null
+    }
   }
-  else
-  {
-    null
+
+  def gestionCompte(userId: Int): Int = {
+    println("|Mon compte")
+    val menu = List("Modifier le mot de passe", "Modifier le nom d'utilisateur", "Modifier le vehicule", "Supprimer le compte", "Quitter")
+
+    for ((section, index) <- menu.zipWithIndex) {
+      println(s"${index + 1} : $section")
+    }
+
+    val choix = StdIn.readInt()
+
+    choix match {
+      case 1 =>
+        var password: String = null
+        var passwordOk = false
+        while (!passwordOk) {
+          println("Choisissez votre nouveau mot de passe:")
+          password = StdIn.readLine()
+          println("Confirmez votre nouveau mot de passe")
+          if (password == StdIn.readLine())
+            passwordOk = true
+          else
+            println("/!\\Les mots de passe ne correspondent pas")
+        }
+        val requete = "UPDATE USER SET user_hash_pwd = ? WHERE user_id = ?"
+        try {
+          val statement = DBConnection.connection.prepareStatement(requete)
+          statement.setString(1, password)
+          statement.setInt(2, userId)
+          statement.executeUpdate()
+          println("///Mise à jour du mot de passe réussie ")
+          userId
+        }
+        catch {
+          case e: Exception => e.printStackTrace()
+            println("Echec lors de la mise à jour du mot de passe")
+            userId
+        }
+      case 2 =>
+        println("Choisissez votre nouveau nom")
+        var userName = StdIn.readLine()
+
+        val requete = "UPDATE USER SET user_name = ? WHERE user_id = ?"
+        try {
+          val statement = DBConnection.connection.prepareStatement(requete)
+          statement.setString(1, userName)
+          statement.setInt(2, userId)
+          statement.executeUpdate()
+          println("///Mise à jour du nom réussie ")
+          userId
+        }
+        catch {
+          case e: Exception => e.printStackTrace()
+            println("Echec lors de la mise à jour du nom")
+            userId
+        }
+      case 3 =>
+        println("Choisissez votre nouveu vehicule")
+        var userName = StdIn.readLine()
+
+        val requete = "UPDATE USER SET user_vehicule = ? WHERE user_id = ?"
+        try {
+          val statement = DBConnection.connection.prepareStatement(requete)
+          statement.setString(1, userName)
+          statement.setInt(2, userId)
+          statement.executeUpdate()
+          println("///Mise à jour du vehicule réussie")
+          userId
+        }
+        catch {
+          case e: Exception => e.printStackTrace()
+            println("Echec lors de la mise à jour du vehicule")
+            userId
+        }
+      case 4 =>
+        println("Confirmer la suppression ?")
+        println("1: Non")
+        println("2: Oui")
+        val choix = StdIn.readInt()
+        choix match {
+          case 1 =>
+            println("Annulation de la suppression")
+            userId
+          case 2 =>
+            try {
+              val requete = "DELETE FROM USER WHERE user_id = ?"
+              val statement = DBConnection.connection.prepareStatement(requete)
+              statement.setInt(1,userId)
+              statement.executeUpdate()
+              println("Suppression du compte réussie")
+              0
+            }catch{
+              case e: Exception => e.printStackTrace()
+                println("Echec lors de la suppression du compte")
+                userId
+            }
+        }
+      case _ =>
+        println("Commande invalide !")
+        userId
+    }
+  }
+
+  def userFindById(id: Int): Option[User] = {
+    val requete = "SELECT * FROM User WHERE user_id = ?"
+    val statement = DBConnection.connection.prepareStatement(requete)
+    statement.setInt(1, id)
+    val result = statement.executeQuery()
+
+    if (result.next()) {
+      Some(User(
+        userId = result.getInt("user_id"),
+        nom = result.getString("user_name"),
+        vehicule = result.getString("user_vehicule"),
+        note = result.getInt("user_note"),
+        nombreNote = result.getInt("user_nb_notes")
+      ))
+    } else {
+      None
+    }
+  }
+
+  def findAll(): List[User] = {
+    val requete = "SELECT * FROM User"
+    val statement = DBConnection.connection.prepareStatement(requete)
+    val result = statement.executeQuery()
+    var users = List[User]()
+    while (result.next()) {
+      val user = User(
+        userId = result.getInt("user_id"),
+        nom = result.getString("user_name"),
+        vehicule = result.getString("user_vehicule"),
+        note = result.getInt("user_note"),
+        nombreNote = result.getInt("user_nb_notes")
+      )
+      users = user :: users
+    }
+    users.reverse
   }
 }
-
-
